adc76481d7bc917853611f01cfd996cf004cd193
Darosss <Daroing12@gmail.com>
2022-08-05 23:42:59 +0200

    Improved emotes on screen, removed debug

--- InGameChatStreamelements/main.js
+++ InGameChatStreamelements/main.js
@@ -20,6 +20,7 @@ let userCurrency,
 
 window.addEventListener("onEventReceived", function (obj) {
   chatter = obj.detail.event.data;
+
   let nickname = chatter.nick;
   let msg = chatter.text;
   let msg_time_txt = null;
@@ -37,23 +38,19 @@ window.addEventListener("onEventReceived", function (obj) {
     let first_index = 0;
 
     for (let i = 0; i < chatter.emotes.length; i++) {
-      msg = message_with_emotes.replace(
+      console.log(chatter.emotes[i].name);
+      message_with_emotes = message_with_emotes.replaceAll(
         chatter.emotes[i].name,
         "<img src='" + chatter.emotes[i].urls[1] + "'>"
       );
     }
+    msg = message_with_emotes;
   }
+  console.log(msg);
   addEvent(nickname, msg, msg_time_txt);
 });
 
 window.addEventListener("onWidgetLoad", function (obj) {
-  //addEvent("TUTAJ nick", "cotamjaktam??\n jak tam? lol wtf hahahaha jakbys tak zrobil cos tam ererererer er sdfsd fsdfsd fsdf sd fsd fsd fsd ", '14:23:32')
-  //addEvent("TUTAJ NICK", "cotamjaktam??\n jak tam? lol wtf hahahaha jakbys tak zrobil cos tam ererererer er sdfsd fsdfsd fsdf sd fsd fsd fsd ", '14:23:32')
-  //addEvent("TUTAJ NICK", "cotamjaktam??\n jak tam? lol wtf hahahaha jakbys tak zrobil cos tam ererererer er sdfsd fsdfsd fsdf sd fsd fsd fsd ", '14:23:32')
-  //addEvent("TUTAJ NICK", "cotamjd ")
-  //addEvent("TUTAJ NICK", "cotam")
-  //addEvent("TUTAJ NICK", "cotamjaktam??\n jak tam? lol wtf hahahaha jakbys tak zrobil cos ta? lol wtf hahahaha jakbys tak zrobil cos ta? lol wtf hahahaha jakbys tak zrobil cos ta? lol wtf hahahaha jakbys tak zrobil cos tam ercotamjaktam??\n jak tam? lol wtf hahahaha jakbys tak zrobil cos tam ercotamjaktam??\n jak tam? lol wtf hahahaha jakbys tak zrobil cos tam ererererer er sdfsd fsdfsd fsdf sd fsd fsd fsd ")
-  //addEvent("tyseK", "<img src='https://static-cdn.jtvnw.net/emoticons/v2/25/default/dark/1.0'> Siema <img src='https://static-cdn.jtvnw.net/emoticons/v2/425618/default/dark/1.0'> <img src='https://cdn.betterttv.net/emote/54fa8f1401e468494b85b537/1x'> co tamKappa Siema <img src='https://static-cdn.jtvnw.net/emoticons/v2/425618/default/dark/1.0'> <img src='https://cdn.betterttv.net/emote/54fa8f1401e468494b85b537/1x'> co tamKappa Siema <img src='https://static-cdn.jtvnw.net/emoticons/v2/425618/default/dark/1.0'> <img src='https://cdn.betterttv.net/emote/54fa8f1401e468494b85b537/1x'> co tamKappa Siema <img src='https://static-cdn.jtvnw.net/emoticons/v2/425618/default/dark/1.0'> <img src='https://cdn.betterttv.net/emote/54fa8f1401e468494b85b537/1x'> co tamKappa Siema <img src='https://static-cdn.jtvnw.net/emoticons/v2/425618/default/dark/1.0'> <img src='https://cdn.betterttv.net/emote/54fa8f1401e468494b85b537/1x'> co tamKappa Siema <img src='https://static-cdn.jtvnw.net/emoticons/v2/425618/default/dark/1.0'> <img src='https://cdn.betterttv.net/emote/54fa8f1401e468494b85b537/1x'> co tamKappa Siema <img src='https://static-cdn.jtvnw.net/emoticons/v2/425618/default/dark/1.0'> <img src='https://cdn.betterttv.net/emote/54fa8f1401e468494b85b537/1x'> co tam ")
   let recents = obj.detail.recents;
   recents.sort(function (a, b) {
     return Date.parse(a.createdAt) - Date.parse(b.createdAt);
@@ -72,6 +69,7 @@ function addEvent(username, text, hour) {
   let element;
   let usernameDiv = `<div class="username-container">${username}</div>`;
   if (hour) {
+    console.log("ta");
     usernameDiv += ` <div class="msg-time-container">${hour}</div>`;
   }
   element = `
@@ -84,9 +82,6 @@ function addEvent(username, text, hour) {
   } else {
     $(".main-container").removeClass("fadeOutClass").show().prepend(element);
   }
-  //if (fadeoutTime !== 999) {
-  //    $('.main-container').addClass("fadeOutClass");
-  // }
   if (totalEvents > eventsLimit) {
     removeEvent(totalEvents - eventsLimit);
   }
